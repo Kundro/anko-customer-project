e82a157958228d5c1049201b8bed58daf4c76057
Alexey Kundro <exeykundro@gmail.com>
2022-08-16 11:34:58 +0300

    test: configure unit tests

--- Customer/Customer.Tests/AddressTests.cs
+++ Customer/Customer.Tests/AddressTests.cs
@@ -1,3 +1,4 @@
+using CustomerLibrary.Entities;
 using System;
 using Xunit;
 
@@ -6,9 +7,34 @@ namespace CustomerLibrary.Tests
     public class AddressTests
     {
         [Fact]
-        public void Test1()
+        public void ShouldBeAbleToCreateAddress()
         {
+            Address address = new Address("line1", "line2", AddressType.Shipping);
+            Assert.NotNull(address.AddressLine);
+            Assert.Equal("line1", address.AddressLine);
+        }
 
+        [Fact]
+        public void ShouldBeEqualAddressLine()
+        {
+            Address address = new Address("line1", "line2", AddressType.Shipping);
+            Assert.NotNull(address.AddressLine);
+            Assert.Equal("line1", address.AddressLine);
+        }
+
+        [Fact]
+        public void ShouldBeEqualAddressLine2()
+        {
+            Address address = new Address("line1", "line2", AddressType.Shipping);
+            Assert.NotNull(address.AddressLine2);
+            Assert.Equal("line2", address.AddressLine2);
+        }
+
+        [Fact]
+        public void ShouldBeEqualAddressType()
+        {
+            Address address = new Address("line1", "line2", AddressType.Shipping);
+            address.AddressType.Equals(AddressType.Shipping);
         }
     }
 }
--- Customer/Customer/Entities/Address.cs
+++ Customer/Customer/Entities/Address.cs
@@ -4,8 +4,23 @@ using System.Text;
 
 namespace CustomerLibrary.Entities
 {
+    public enum AddressType
+    {
+        Shipping,
+        Billing
+    }
     public class Address
     {
+        public Address(string addressLine, string addressLine2, AddressType addressType)
+        {
+            AddressLine = addressLine;
+            AddressLine2 = addressLine2;
+            AddressType = addressType;     
+        }
 
+        public string AddressLine { get; set; }
+        public string AddressLine2 { get; set; }
+        public AddressType AddressType { get; set; }
+        
     }
 }
