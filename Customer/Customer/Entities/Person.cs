258396e7711040c37d42f5cce6e4e4a25635f8c2
Alexey Kundro <exeykundro@gmail.com>
2022-08-17 13:22:03 +0300

    ref: some code refactoring

--- Customer/Customer/Entities/Person.cs
+++ Customer/Customer/Entities/Person.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace CustomerLibrary.Entities
+﻿namespace CustomerLibrary.Entities
 {
     public abstract class Person
     {
