f28a76ccbdcaf47f1643836ee1c12fc8b6555a94
Alexey Kundro <exeykundro@gmail.com>
2022-08-17 11:51:00 +0300

    test: add AddressValidator and test it

--- Customer/Customer.Tests/AddressValidatorTests.cs
+++ Customer/Customer.Tests/AddressValidatorTests.cs
@@ -1,4 +1,5 @@
-using System;
+using CustomerLibrary.Entities;
+using CustomerLibrary.Validators;
 using Xunit;
 
 namespace CustomerLibrary.Tests
@@ -6,9 +7,83 @@ namespace CustomerLibrary.Tests
     public class AddressValidatorTests
     {
         [Fact]
-        public void Test2()
+        public void ShouldReturnLongAddressLine()
         {
+            Address address = new Address(new string('1', 101), "line2", AddressType.Shipping, "Chicago", "60666", "Illinois", "USA");
+            var current = AddressValidator.Validate(address);
+            Assert.Contains("Address line is too long", current);
+        }
 
+        [Fact]
+        public void ShouldReturnWrongAddressLine()
+        {
+            Address address = new Address("", "line2", AddressType.Shipping, "Chicago", "60666", "Illinois", "USA");
+            var current = AddressValidator.Validate(address);
+            Assert.Contains("Address line required", current);
+        }
+
+        [Fact]
+        public void ShouldReturnLongAddressLine2()
+        {
+            Address address = new Address("line", new string('2', 101), AddressType.Shipping, "Chicago", "60666", "Illinois", "USA");
+            var current = AddressValidator.Validate(address);
+            Assert.Contains("Address line 2 is too long", current);
+        }
+
+        [Fact]
+        public void ShouldReturnLongCity()
+        {
+            Address address = new Address("line", "line2", AddressType.Shipping, new string('c', 51), "60666", "Illinois", "USA");
+            var current = AddressValidator.Validate(address);
+            Assert.Contains("City name is too long", current);
+        }
+
+        [Fact]
+        public void ShouldReturnLongPostalCode()
+        {
+            Address address = new Address("line", "line2", AddressType.Shipping, "Chicago", new string('c', 7), "Illinois", "USA");
+            var current = AddressValidator.Validate(address);
+            Assert.Contains("Postal code is too long", current);
+        }
+
+        [Fact]
+        public void ShouldNotReturnCity()
+        {
+            Address address = new Address("line", "line2", AddressType.Shipping, "", "60666", "Illinois", "USA");
+            var current = AddressValidator.Validate(address);
+            Assert.Contains("City name required", current);
+        }
+
+        [Fact]
+        public void ShouldNotReturnPostalCode()
+        {
+            Address address = new Address("line", "line2", AddressType.Shipping, "Chicago", "", "Illinois", "USA");
+            var current = AddressValidator.Validate(address);
+            Assert.Contains("Postal code required", current);
+        }
+
+        [Fact]
+        public void ShouldNotReturnState()
+        {
+            Address address = new Address("line", "line2", AddressType.Shipping, "Chicago", "60666", "", "USA");
+            var current = AddressValidator.Validate(address);
+            Assert.Contains("State name required", current);
+        }
+
+        [Fact]
+        public void ShouldReturnLongState()
+        {
+            Address address = new Address("line", "line2", AddressType.Shipping, "Chicago", "60666", new string('c', 21), "USA");
+            var current = AddressValidator.Validate(address);
+            Assert.Contains("State name is too long", current);
+        }
+
+        [Fact]
+        public void ShouldReturnWrongCountry()
+        {
+            Address address = new Address("line", "line2", AddressType.Shipping, "Chicago", "60666", "Illinois", "Belarus");
+            var current = AddressValidator.Validate(address);
+            Assert.Contains("Wrong country name", current);
         }
     }
 }
--- Customer/Customer/Validators/AddressValidator.cs
+++ Customer/Customer/Validators/AddressValidator.cs
@@ -1,9 +1,79 @@
-﻿using System;
+﻿using CustomerLibrary.Entities;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CustomerLibrary.Validators
 {
     public class AddressValidator
     {
+        static readonly string[] COUNTRIES =
+        {
+            "USA",
+            "united states",
+            "United States",
+            "United States of America",
+            "Canada",
+            "canada"
+        };
+        const string ADDRESS_LINE_LONG = "Address line is too long";
+        const string ADDRESS_LINE_2_LONG = "Address line 2 is too long";
+        const string ADDRESS_LINE_REQ = "Address line required";
+        const string CITY_LONG = "City name is too long";
+        const string CITY_REQ = "City name required";
+        const string POSTAL_CODE_LONG = "Postal code is too long";
+        const string POSTAL_REQ = "Postal code required";
+        const string STATE_REQ = "State name required";
+        const string STATE_LONG = "State name is too long";
+        const string WRONG_COUNTRY = "Wrong country name";
 
+
+
+        public static List<string> Validate(Address address)
+        {
+            var errors = new List<string>();
+
+            if (address.AddressLine.Length > 100)
+            {
+                errors.Add(ADDRESS_LINE_LONG);
+            }
+            if (string.IsNullOrWhiteSpace(address.AddressLine))
+            {
+                errors.Add(ADDRESS_LINE_REQ);
+            }
+            if (address.AddressLine2.Length > 100)
+            {
+                errors.Add(ADDRESS_LINE_2_LONG);
+            }
+            if (address.City.Length > 50)
+            {
+                errors.Add(CITY_LONG);
+            }
+            if(address.PostalCode.Length > 6)
+            {
+                errors.Add(POSTAL_CODE_LONG);
+            }
+            if (string.IsNullOrWhiteSpace(address.City))
+            {
+                errors.Add(CITY_REQ);
+            }
+            if (string.IsNullOrWhiteSpace(address.PostalCode))
+            {
+                errors.Add(POSTAL_REQ);
+            }
+            if (string.IsNullOrWhiteSpace(address.State))
+            {
+                errors.Add(STATE_REQ);
+            }
+            if (address.State.Length > 20)
+            {
+                errors.Add(STATE_LONG);
+            }
+            if (!COUNTRIES.Contains(address.Country))
+            {
+                errors.Add(WRONG_COUNTRY);
+            }
+
+            return errors;
+        }
     }
 }
